dd326e04d4e454d42f642c79418d353e345867b5
Philipp Jungkamp <p.jungkamp@gmx.net>
2024-04-05 21:45:59 +0200

    Remove all dependencies

--- flake.nix
+++ flake.nix
@@ -1,36 +1,7 @@
 {
   description = "";
 
-  inputs = {
-    nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
-
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
-    };
+  outputs = {...}: {
+    flakeModules.default = import ./flake-module.nix;
   };
-
-  outputs = inputs @ {
-    self,
-    flake-parts,
-    nixpkgs-lib,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} ({
-      flake-parts-lib,
-      self,
-      ...
-    }: let
-      lib = import ./lib.nix {
-        inherit (nixpkgs-lib) lib;
-      };
-
-      flakeModules.default = import ./flake-module.nix;
-    in {
-      systems = ["x86_64-linux"];
-
-      flake = {
-        inherit flakeModules lib;
-      };
-    });
 }
